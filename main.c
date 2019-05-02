9eb082b9ce235e08907d4d339b0da6d1b4179c73
Atakan Guven <50146105+atakannguvenn@users.noreply.github.com>
2019-05-02 10:57:52 +0800

    Update main.c

--- main.c
+++ main.c
@@ -21,55 +21,53 @@ int getMax(int arr[], int size){
 
 void insertionSort(int arr[][12], int bucket, int n)
 {
-   int i, key, j, z;
-   for (z = 0; z < bucket; z++)
-   {
-        for (i = 1; i < n; i++)
-        {
-            if(arr[z][i] == NULL)
-                continue;
-            key = arr[z][i];
-            j = i-1;
-            while (j >= 0 && arr[z][j] > key)
-            {
-                arr[z][j+1] = arr[z][j];
-                j = j-1;
-            }
-            arr[z][j+1] = key;
-        }
-   }
+	int i, key, j, z;
+	for (z = 0; z < bucket; z++)
+	{
+		for (i = 1; i < n; i++)
+		{
+			if(arr[z][i] == NULL)
+				continue;
+			key = arr[z][i];
+			j = i-1;
+			while (j >= 0 && arr[z][j] > key)
+			{
+				arr[z][j+1] = arr[z][j];
+				j = j-1;
+			}
+			arr[z][j+1] = key;
+		}
+	}
 }
 
 void bucketSort(int arr[], int size){
 	int max, bucket = 10, divider, i, j, k, n, b;
-    float div, mx, buc;
+    	float div, mx, buc;
 	int B[bucket][size];
-
 	for (i = 0; i < bucket; i++){
-        for (b = 0; b < size; b++)
-            B[i][b] = NULL;
+		for (b = 0; b < size; b++)
+            		B[i][b] = NULL;
 	}
 	max = getMax(arr, size);
-    div = divider;
+    	div = divider;
 	buc = bucket;
 	div = (max + 1) / buc;
-
 	printf("\nAfter Building the Bucket\nDivider is %.2f\n", div);
 	for(i = 0; i < size; i++){
-        n = 0;
+        	n = 0;
 		j = arr[i] / div;
 		while (B[j][n]!= NULL)
-            n++;
+            		n++;
 		B[j][n] = arr[i];
 		printf("B[%d][%d] = %d\n", j, n, B[j][n]);
 	}
 	printf("\n");
-    insertionSort(B, 10, 12);
+    	insertionSort(B, 10, 12);
 	k = 0;
 	for(i = 0; i < bucket; i++){
 		for(j = 0; j < size; j++){
-            if (B[i][j] == NULL)
-                continue;
+            		if (B[i][j] == NULL)
+                		continue;
 			arr[k++] = B[i][j];
 		}
 	}
